4cab6292d1e305f4bc73d3929b815d400a63b1f8
alex1431999 <alexhaller99@yahoo.de>
2024-09-04 16:36:14 +0200

    make map bigger

--- src/maps/map.first.ts
+++ src/maps/map.first.ts
@@ -1,7 +1,12 @@
 import {MapLayout} from "./maps.types.js";
 
 export const MAP_FIRST: MapLayout = [
-    ['pl', 'pl', 'pl', 'pl'],
-    ['pa', 'pa', 'pa', 'pa'],
-    ['pl', 'pl', 'pl', 'pl'],
+    ['pl', 'pl', 'pl', 'pl', 'pl', 'pl'],
+    ['pa', 'pa', 'pa', 'pa', 'pa', 'pl'],
+    ['pl', 'pl', 'pl', 'pl', 'pa', 'pl'],
+    ['pl', 'pl', 'pa', 'pa', 'pa', 'pl'],
+    ['pl', 'pl', 'pa', 'pl', 'pl', 'pl'],
+    ['pl', 'pl', 'pa', 'pa', 'pa', 'pl'],
+    ['pl', 'pl', 'pl', 'pl', 'pa', 'pl'],
+    ['pl', 'pl', 'pl', 'pl', 'pa', 'pl'],
 ]

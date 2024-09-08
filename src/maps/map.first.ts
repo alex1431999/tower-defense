2da2454720a6814be15e6dca6cff4161134a99e3
alex1431999 <alexhaller99@yahoo.de>
2024-09-08 20:03:43 +0800

    feat: integrate waves with map

--- src/maps/map.first.ts
+++ src/maps/map.first.ts
@@ -1,5 +1,7 @@
 import {MapLayout} from "./maps.types.js";
 import {GameMap} from "./map.js";
+import {Wave} from "../waves/wave";
+import {WaveFirst1} from "../waves/maps/first/wave.first.1";
 
 export class MapFirst extends GameMap {
     public layout: MapLayout = [
@@ -12,4 +14,8 @@ export class MapFirst extends GameMap {
         ['pl', 'pl', 'pl', 'pl', 'pa', 'pl'],
         ['pl', 'pl', 'pl', 'pl', 'pa', 'pl'],
     ]
+
+    public get waves(): Wave[] {
+        return [new WaveFirst1(this.config)]
+    }
 }
--- src/maps/map.ts
+++ src/maps/map.ts
@@ -1,5 +1,19 @@
 import {MapLayout} from "./maps.types.js";
+import {Wave} from "../waves/wave";
+import {ElementCreature} from "../elements/creatures/creature.js";
+
+export type GameMapConfig = {
+    onDeployCreature: (creature: ElementCreature) => void
+}
 
 export abstract class GameMap {
     public abstract layout: MapLayout
+
+    public abstract get waves(): Wave[]
+
+    public config: GameMapConfig
+
+    constructor(config: GameMapConfig) {
+        this.config = config
+    }
 }
